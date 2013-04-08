93b1ee06c602880658843a2d826a4c2a35808c02
MWirth <mwirth@meyle-mueller.de>
2013-04-08 15:30:08 +0200

    moved TODO Tasks

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiply.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiply.java
@@ -6,13 +6,11 @@ public class Multiply extends AbstractOperation implements BinaryOperation {
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "*";
 	}
 
 	@Override
 	public float perform(float arg1, float arg2) {
-		// TODO Auto-generated method stub
 		return arg1 + arg2;
 	}
 
