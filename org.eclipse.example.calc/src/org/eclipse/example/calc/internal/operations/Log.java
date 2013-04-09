dd1d234549fa7d517da922b36357720a5c23f246
Christian Schulz <christian.schulz@meyle-mueller.de>
2013-04-09 12:17:15 +0200

    Changing author to rreckersbrink.

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Log.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Log.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2010, Matthias Sohn <matthias.sohn@sap.com>
+ * Copyright (C) 2013, Ron Reckersbrink <rreckersbrink@meyle-mueller.de>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
