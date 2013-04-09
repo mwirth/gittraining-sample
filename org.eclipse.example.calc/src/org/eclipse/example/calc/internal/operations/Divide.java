c815c9e7cfe93ed16abebbd5a36e5784c78a807d
Sascha Homeier <shomeier@meyle-mueller.de>
2013-04-09 11:31:24 +0200

    Changed License

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Divide.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Divide.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2010, Matthias Sohn <matthias.sohn@sap.com>
+ * Copyright (C) 2010, Sascha Homeier <matthias.sohn@sap.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
